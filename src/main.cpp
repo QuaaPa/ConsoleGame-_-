31ab9479996c0234285eb084599ab181cde1745c
Vadim Kolnoguz <vadimkolno@gmail.com>
2023-09-01 18:03:48 +0200

    Добавьте файлы проекта.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include "Map/map.hpp"
+#include "render/rendering.hpp"
+
+int main()
+{
+    //Load default map from txt file "map.txt to vector "map" 
+    Map defaultMap;
+    defaultMap.ReadMap();
+
+    Rendering window;
+    window.setmap(defaultMap.map);
+
+    window.renderMap();
+
+
+    return 0;
+}
